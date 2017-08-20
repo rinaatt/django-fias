422024711cffc00817cba03c8a8d924e507c91e9
Rinat R. Akhtamov <rinaatt@gmail.com>
2017-08-20 21:57:59 +0500

    Добавлены локальные настройки для `ext_db_pg_settings.py`

--- test_project/test_project/ext_db_pg_settings.py
+++ test_project/test_project/ext_db_pg_settings.py
@@ -2,13 +2,18 @@ from .settings import *
 
 
 DATABASES['fias'] = {
-   'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   'NAME': 'fias_test',
-   'USER': 'postgres',
-   'PASSWORD': '',
-   'HOST': '127.0.0.1',
-   'PORT': '5432',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'fias_test',
+    'USER': 'postgres',
+    'PASSWORD': '',
+    'HOST': '127.0.0.1',
+    'PORT': '5432',
 }
 
 FIAS_DATABASE_ALIAS = 'fias'
 DATABASE_ROUTERS = ['fias.routers.FIASRouter']
+
+try:
+    from .ext_db_pg_settings_local import *
+except ImportError:
+    pass
